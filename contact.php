f0344b46b1704fc4cffe151f9d8287175b013fe3
sergioblancoo <sergioblancoo@gmail.com>
2020-12-07 22:55:06 +1100

    Update Contact php

--- contact.php
+++ contact.php
@@ -41,26 +41,18 @@ if(get_magic_quotes_gpc()) {
 
 
 // Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
 
-//$address = "example@themeforest.net";
-$address = "example@yourdomain.com";
+$address = "info@secretamigoapp.com";
 
 
 // Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
 $e_subject = 'You\'ve been contacted by ' . $first_name . '.';
 
 
 // Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
+$e_body = "You have been contacted by $first_name." . PHP_EOL . PHP_EOL;
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $first_name via email, $email or via phone $phone";
 
